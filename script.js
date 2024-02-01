9f52be4df05bb02ff2759b402c619994dfdfebb6
abnderrazak-errifaouy <72007631+abderrazakerrifaouy@users.noreply.github.com>
2024-02-01 23:34:18 +0100

    hh
    hioil

---
+++ script.js
@@ -0,0 +1,56 @@
+
+  var audio = document.getElementById("audioPlayer");
+  var playButton = document.getElementById("playButton");
+
+  playButton.addEventListener("click", function() {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  });
+
+  var audio1 = document.getElementById("audioPlayer2");
+  var playButton1 = document.getElementById("playButton2");
+
+  playButton1.addEventListener("click", function() {
+    if (audio1.paused) {
+      audio1.play();
+    } else {
+      audio1.pause();
+    }
+  });
+
+  var audio3 = document.getElementById("audioPlayer3");
+  var playButton3 = document.getElementById("playButton3");
+
+  playButton3.addEventListener("click", function() {
+    if (audio3.paused) {
+      audio3.play();
+    } else {
+      audio3.pause();
+    }
+  });
+
+  var audio4 = document.getElementById("audioPlayer4");
+  var playButton4 = document.getElementById("playButton4");
+
+  playButton4.addEventListener("click", function() {
+    if (audio4.paused) {
+      audio4.play();
+    } else {
+      audio4.pause();
+    }
+  });
+
+  var audio5 = document.getElementById("audioPlayer5");
+  var playButton5 = document.getElementById("playButton5");
+
+  playButton5.addEventListener("click", function() {
+    if (audio5.paused) {
+      audio5.play();
+    } else {
+      audio5.pause();
+    }
+  });
+
